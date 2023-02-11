5861b26c2ca2263c6732f8e0d14669ad1855370c
Zach <121370147+zjmstudio@users.noreply.github.com>
2023-02-11 03:07:38 -0500

    Update Account.js

--- routes/Account.js
+++ routes/Account.js
@@ -19,18 +19,18 @@ const handleSignOut = async () => {
 if (user) {
   return (
     <div className='max-w-[1140px] mx-auto'>
-      <div className='flex justify-between items-center my-12 py-8 rounded-div'>
+      <div className='account-box'>
         <div>
-            <h1 className='text-2xl font-bold'>Account</h1>
+            <h1 className='text-2xl font-bold py-4'>Account</h1>
             <div>
                 <p>Welcome, {user?.email}</p>
             </div>
         </div>
         <div>
-            <button onClick={handleSignOut} className='btn'>Sign Out</button>
+            <button onClick={handleSignOut} className='account-btn'>Sign Out</button>
         </div>
       </div>
-      <div className='flex justify-between items-center my-12 py-8 rounded-div'>
+      <div className='account-box'>
         <div className='w-full min-h-[300px]'>
             <h1 className='text-2xl font-bold py-4'>Watch List</h1>
             <SavedCoin />
